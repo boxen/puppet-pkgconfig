f99abf2493c4af2efd7133a0b50f7b1623844203
telamonian <mklein@jhu.edu>
2014-08-23 19:27:34 -0400

    modernized (as per puppet-template) and added tests

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,21 @@
+# Public: Install homebrew version of pkg-config that uses the correct pc_path for .pc file lookup
+#
+# Examples
+#
+#   include pkgconfig
 class pkgconfig {
-  if $::osfamily == "Darwin" {
-	  package {
-	    'gettext': ;
-	    'pkg-config':
-	      require => Package['gettext'];
-	  }
-	}
-	elsif $::osfamily == "Debian" {
-	  homebrew::formula { 'pkg-config': 
-	    before => Package[ 'boxen/brews/pkg-config' ]
-	  }
+  if $::osfamily == 'Darwin' {
+    package {
+      'gettext': ;
+      'pkg-config':
+        require => Package['gettext'];
+    }
+  }
+  elsif $::osfamily == 'Debian' {
+    homebrew::formula { 'pkg-config':
+      before => Package[ 'boxen/brews/pkg-config' ]
+    }
 
     package { 'boxen/brews/pkg-config': }
-	}
+  }
 }
